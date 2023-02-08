7849ac7f07e08d5fac9ef23af9d7bb253928b2b7
Boris Rozov <outsource@list.ru>
2023-02-08 22:01:33 +0300

    Доработка 1

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,53 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.*;
+import java.util.*;
 
 public class Main {
     // Коллекция комбинаций повторов и их частоты
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+    public static final List<Thread> myThread = new ArrayList<>();
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService threadPool = Executors.newFixedThreadPool(1000);
+    public static void main(String[] args) throws InterruptedException {
         int keyMax = 0;    // Комбинация повторов встречающаяся максимальное количество раз
         int valueMax = 0;  // Значение максимального количества повторов комбинации
 
-        Callable<Integer> call = () -> calcR(generateRoute("RLRFR", 100));
         // Заполнение коллекции комбинаций повторов и их частоты
-        for (int i = 1; i < 1000; i++) {
-            Future<Integer> future = threadPool.submit(call);
-            int countR = future.get();
-            synchronized (sizeToFreq) {
-                if (sizeToFreq.isEmpty()) {
-                    sizeToFreq.put(countR, 1);
-                } else {
-                    if (sizeToFreq.containsKey(countR)) {
-                        sizeToFreq.put(countR, sizeToFreq.get(countR) + 1);
-                    } else sizeToFreq.putIfAbsent(countR, 1);
+        for (int i = 1; i < 10; i++) {
+            new Thread(() -> {
+                synchronized (myThread)  { myThread.add(Thread.currentThread());}
+                System.out.println("Float " + Thread.currentThread().getName() + " started.....");
+                final int countR = calcR(generateRoute("RLRFR", 100));
+                synchronized (sizeToFreq) {
+                    if (sizeToFreq.isEmpty()) {
+                        sizeToFreq.put(countR, 1);
+                    } else {
+                        if (sizeToFreq.containsKey(countR)) {
+                            sizeToFreq.put(countR, sizeToFreq.get(countR) + 1);
+                        } else sizeToFreq.putIfAbsent(countR, 1);
+                    }
+                }
+            }).start();
+        }
+            synchronized (myThread) {
+                for (Thread thread : myThread) {
+                    thread.join();
+                    System.out.println("Flow " + thread.getName() + " is done.");
                 }
             }
-        }
         // Поиск и вывод в консоль значения максимального количества повторений
-        for (Map.Entry<Integer, Integer> key1 : sizeToFreq.entrySet()) {
-            if (key1.getValue() > valueMax) {
-                valueMax = key1.getValue();
-                keyMax = key1.getKey();
+        synchronized (sizeToFreq) {
+            for (Map.Entry<Integer, Integer> key1 : sizeToFreq.entrySet()) {
+                if (key1.getValue() > valueMax) {
+                    valueMax = key1.getValue();
+                    keyMax = key1.getKey();
+                }
             }
         }
         System.out.println("Самое частое количество повторений " + keyMax + " встретилось (" + valueMax + " раз)");
         // Вывод в консоль перечня комбинаций повторов и их частоты
-        for (Map.Entry<Integer, Integer> key1 : sizeToFreq.entrySet()) {
-            System.out.println("- " + key1.getKey() + " (" + key1.getValue() + " раз)");
+        synchronized (sizeToFreq) {
+            for (Map.Entry<Integer, Integer> key1 : sizeToFreq.entrySet()) {
+                System.out.println("- " + key1.getKey() + " (" + key1.getValue() + " раз)");
+            }
         }
-        threadPool.shutdown(); // Закрытие потоков
     }
 
     // Метод генерации строк
